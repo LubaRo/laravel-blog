6fc16f40e0d4290063fd28c2db0394e3b360204b
Lyubov Parfilova <cnlmode@gmail.com>
2021-05-23 10:03:44 +0400

    seed the db with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,54 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = \App\Models\User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal-category'
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Joe Doe'
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work-category'
-        ]);
-
-        $hobby = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies-category'
-        ]);
-
-        $body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-
-        Post::create([
-            'title' => 'My personal post',
-            'category_id' => $personal->id,
-            'user_id' => $user->id,
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Short description',
-            'body' => $body
-        ]);
-
-        Post::create([
-            'title' => 'My work post',
-            'category_id' => $work->id,
-            'user_id' => $user->id,
-            'slug' => 'first-work-post',
-            'excerpt' => 'Short description',
-            'body' => $body
-        ]);
-
-        Post::create([
-            'title' => 'My hobby post',
-            'category_id' => $hobby->id,
-            'user_id' => $user->id,
-            'slug' => 'my-hobby-post',
-            'excerpt' => 'Short description',
-            'body' => $body
+        \App\Models\Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
